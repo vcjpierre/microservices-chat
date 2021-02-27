842fc50fbca1a625a26769e9d895f45eea84cb58
Jean Pierre Velez <vcjpierre@gmail.com>
2021-02-27 13:11:24 -0400

    Creating a GraphQL Mutation

--- api-gateway/src/graphql/schema.ts
+++ api-gateway/src/graphql/schema.ts
@@ -2,16 +2,24 @@ import { gql } from "apollo-server";
 
 const schema = gql`
   scalar Date
+
   type User {
     id: ID!
     username: String!
   }
+
   type UserSession {
     createdAt: Date!
     expiresAt: Date!
     id: ID!
     user: User!
   }
+
+  type Mutation {
+    createUser(password: String!, username: String!): User!
+    createUserSession(password: String!, username: String!): UserSession!
+  }
+
   type Query {
     userSession(me: Boolean!): UserSession
   }
