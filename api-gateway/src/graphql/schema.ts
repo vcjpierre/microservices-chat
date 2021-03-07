29e97c5a21f3b862270756b730fb0b09e1bf009a
Jean Pierre Velez <vcjpierre@gmail.com>
2021-03-07 02:18:16 -0400

    Logging the user out and refactoring GraphQL

--- api-gateway/src/graphql/schema.ts
+++ api-gateway/src/graphql/schema.ts
@@ -6,18 +6,19 @@ const schema = gql`
   type User {
     id: ID!
     username: String!
+    username: ID!
   }
 
   type UserSession {
     createdAt: Date!
     expiresAt: Date!
-    id: ID!
     user: User!
   }
 
   type Mutation {
     createUser(password: String!, username: String!): User!
     createUserSession(password: String!, username: String!): UserSession!
+    deleteUserSession(me: Boolean!): Boolean!
   }
 
   type Query {
